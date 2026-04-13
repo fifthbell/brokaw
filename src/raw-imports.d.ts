20876beb3215728b900de67a1ed711664f626b0f
Javier Godoy <jack@gaulatti.com>
2026-04-13 13:05:31 -0400

    feat: Instagram template

--- src/raw-imports.d.ts
+++ src/raw-imports.d.ts
@@ -7,3 +7,17 @@ declare module '*.css?raw' {
   const content: string;
   export default content;
 }
+
+declare module 'qrcode-generator' {
+  type ErrorCorrectionLevel = 'L' | 'M' | 'Q' | 'H';
+
+  type QRCode = {
+    addData(data: string): void;
+    make(): void;
+    createSvgTag(cellSize?: number, margin?: number): string;
+  };
+
+  const createQrCode: (typeNumber: number, errorCorrectionLevel: ErrorCorrectionLevel) => QRCode;
+
+  export default createQrCode;
+}
